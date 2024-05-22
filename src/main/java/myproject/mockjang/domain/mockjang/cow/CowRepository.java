8040e286d7b917f29e2c8cad29cb4e80dd845e3d
choc0villain <sunww2017@naver.com>
2024-05-22 20:32:05 +0900

    add : 레포지토리 등록

--- src/main/java/myproject/mockjang/domain/mockjang/cow/CowRepository.java
+++ src/main/java/myproject/mockjang/domain/mockjang/cow/CowRepository.java
@@ -1,4 +1,4 @@
-package myproject.mockjang.domain.cow;
+package myproject.mockjang.domain.mockjang.cow;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
