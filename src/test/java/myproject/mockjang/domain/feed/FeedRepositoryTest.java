f9793873431216afa8e2a2d601b95f164060caa2
choc0villain <sunww2017@naver.com>
2024-05-24 16:31:05 +0900

    refactor

--- src/test/java/myproject/mockjang/domain/feed/FeedRepositoryTest.java
+++ src/test/java/myproject/mockjang/domain/feed/FeedRepositoryTest.java
@@ -1,11 +1,9 @@
 package myproject.mockjang.domain.feed;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDate;
 import myproject.mockjang.IntegrationTestSupport;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
