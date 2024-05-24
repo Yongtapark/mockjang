38f4d945abb1cdbd7d31f473fcf7dded58ddd47c
choc0villain <sunww2017@naver.com>
2024-05-24 16:33:07 +0900

    refactor

--- src/test/java/myproject/mockjang/IntegrationTestSupport.java
+++ src/test/java/myproject/mockjang/IntegrationTestSupport.java
@@ -2,13 +2,19 @@ package myproject.mockjang;
 
 import myproject.mockjang.domain.mockjang.cow.Cow;
 import myproject.mockjang.domain.mockjang.cow.Gender;
+import myproject.mockjang.domain.mockjang.cow.CowStatus;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
 public abstract class IntegrationTestSupport {
+
   protected static Cow createCow(String cowId, Gender gender) {
     return Cow.builder().cowId(cowId).gender(gender).build();
+  }
+
+  protected static Cow createCow(String cowId, Gender gender, CowStatus cowStatus) {
+    return Cow.builder().cowId(cowId).gender(gender).cowStatus(cowStatus).build();
   }
 }
--- src/test/java/myproject/mockjang/domain/feedcomsumption/FeedConsumptionRepositoryTest.java
+++ src/test/java/myproject/mockjang/domain/feedcomsumption/FeedConsumptionRepositoryTest.java
@@ -2,6 +2,7 @@ package myproject.mockjang.domain.feedcomsumption;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import jakarta.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 import myproject.mockjang.IntegrationTestSupport;
