de37a97a970401c77a15e17f0267c0689850e068
choc0villain <sunww2017@naver.com>
2024-05-24 16:30:59 +0900

    refactor

--- src/main/java/myproject/mockjang/domain/feed/FeedRepository.java
+++ src/main/java/myproject/mockjang/domain/feed/FeedRepository.java
@@ -1,7 +1,10 @@
 package myproject.mockjang.domain.feed;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FeedRepository extends JpaRepository<Feed,Long> {
+
+  List<Feed> findAllByUsageStatus(FeedUsageStatus usageStatus);
 
 }
