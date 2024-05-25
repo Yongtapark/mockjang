10e5e89491cf9334e23991002f7e257553d026b5
choc0villain <sunww2017@naver.com>
2024-05-25 14:10:58 +0900

    add : 예외 enum 추가

---
+++ src/main/java/myproject/mockjang/domain/Exceptions.java
@@ -0,0 +1,17 @@
+package myproject.mockjang.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum Exceptions {
+  DOMAIN_NEGATIVE_ERROR("음수 입력 시 예외 발생" ),
+  DOMAIN_ONLY_SLAUGHTERED_ERROR("cowStatus.slaughtered 이외 설정 시 예외"),
+
+  BUSINESS_ONLY_SLAUGHTERED_ERROR("도축된 소만 입력이 가능합니다.");
+
+  final String message;
+
+  Exceptions(String message) {
+    this.message = message;
+  }
+}
