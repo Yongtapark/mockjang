1ebed6e777ce006703495ec820fab9e6639cc2e2
Yong Ta Park <qkrdydxk80@nate.com>
2024-05-27 01:28:55 +0900

    refactor : 목장 클레스 상속

--- src/main/java/myproject/mockjang/exception/cow/CowStatusException.java
+++ src/main/java/myproject/mockjang/exception/cow/CowStatusException.java
@@ -2,9 +2,10 @@ package myproject.mockjang.exception.cow;
 
 import lombok.Getter;
 import myproject.mockjang.exception.Exceptions;
+import myproject.mockjang.exception.MockjangException;
 
 @Getter
-public class CowStatusException extends IllegalArgumentException{
+public class CowStatusException extends MockjangException {
 
   public CowStatusException(Exceptions exception) {
     super(exception.getMessage());
