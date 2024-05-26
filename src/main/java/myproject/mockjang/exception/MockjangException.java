5980a824ff4a0bffac5e89bd7a5ffc6c20e5430b
Yong Ta Park <qkrdydxk80@nate.com>
2024-05-27 01:25:54 +0900

    add : 메시지 생성 로직 추가

---
+++ src/main/java/myproject/mockjang/exception/MockjangException.java
@@ -0,0 +1,13 @@
+package myproject.mockjang.exception;
+
+import myproject.mockjang.domain.mockjang.Mockjang;
+
+public class MockjangException extends IllegalArgumentException{
+    public MockjangException(String message) {
+        super(message);
+    }
+
+    public MockjangException(Mockjang mockjang, Exceptions exception) {
+        this(String.format(exception.getMessage(),mockjang.getClass().getSimpleName()));
+    }
+}
