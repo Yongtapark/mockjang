7f1995f9045b158d34a35d9dd707f038ff431db3
Yong Ta Park <qkrdydxk80@nate.com>
2024-05-27 01:26:22 +0900

    add : 추가 예외, 포맷 메서드 추가

--- src/main/java/myproject/mockjang/exception/Exceptions.java
+++ src/main/java/myproject/mockjang/exception/Exceptions.java
@@ -1,12 +1,19 @@
 package myproject.mockjang.exception;
 
 import lombok.Getter;
+import myproject.mockjang.domain.mockjang.Mockjang;
 
 @Getter
 public enum Exceptions {
   //common
-  DOMAIN_BARN_ALREADY_EXIST("축사가 이미 존재함"),
-  DOMAIN_PEN_ALREADY_EXIST("축사칸이 이미 존재함"),
+  COMMON_NO_UPPER_GROUP("상위 그룹이 존재하지 않음. 현재 그룹 : %s"),
+  COMMON_NO_UNDER_GROUP("하위 그룹이 존재하지 않음. 현재 그룹 : %s"),
+  COMMON_BLANK_STRING("빈 문자열은 입력 불가"),
+  COMMON_STRING_OVER_10("문자열은 10자리를 초과할 수 없음"),
+  //barn
+  DOMAIN_BARN_ALREADY_EXIST("%s(이)가 이미 존재함"),
+  //pen
+  DOMAIN_PEN_ALREADY_EXIST("%s(이)가 이미 존재함"),
   //cow,
   DOMAIN_ONLY_SLAUGHTERED_ERROR("cowStatus.slaughtered 이외 설정 시 예외"),
   //feed
@@ -21,5 +28,9 @@ public enum Exceptions {
 
   Exceptions(String message) {
     this.message = message;
+  }
+
+  public String formatMessage(Class<? extends Mockjang> mockjang) {
+    return String.format(getMessage(),mockjang.getSimpleName());
   }
 }
