9ee7eacb38cf879c5623a9ace116ca15809ff0b9
Yong Ta Park <qkrdydxk80@nate.com>
2024-05-27 00:57:48 +0900

    test : 빈문자, 공백, 글자수 예외 테스트

--- src/test/java/myproject/mockjang/api/service/mockjang/barn/BarnServiceTest.java
+++ src/test/java/myproject/mockjang/api/service/mockjang/barn/BarnServiceTest.java
@@ -1,12 +1,15 @@
 package myproject.mockjang.api.service.mockjang.barn;
 
+import static myproject.mockjang.exception.Exceptions.COMMON_BLANK_STRING;
+import static myproject.mockjang.exception.Exceptions.COMMON_STRING_OVER_10;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import myproject.mockjang.IntegrationTestSupport;
-import myproject.mockjang.api.service.mockjang.barn.BarnService;
 import myproject.mockjang.domain.mockjang.barn.Barn;
 import myproject.mockjang.domain.mockjang.barn.BarnRepository;
+import myproject.mockjang.exception.common.StringException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,8 +56,30 @@ class BarnServiceTest extends IntegrationTestSupport {
     assertThat(barns).hasSize(1);
     assertThat(barns).contains(savedBarn2);
     assertThat(barns).doesNotContain(savedBarn1);
+  }
 
+  @DisplayName("축사 이름에 빈 문자열이 들어올 경우 예외를 발생시킨다.")
+  @Test
+  void createBarnWithEmptyBarnId() {
+    //given // when //then
+    assertThatThrownBy(() -> barnService.createBarn(STRING_EMPTY)).isInstanceOf(StringException.class)
+            .hasMessage(COMMON_BLANK_STRING.getMessage());
+  }
 
+  @DisplayName("축사 이름에 공백만 들어올 경우 예외를 발생시킨다.")
+  @Test
+  void createBarnWithOnlySpaceBarnId() {
+    //given // when //then
+    assertThatThrownBy(() -> barnService.createBarn(STRING_ONLY_SPACE)).isInstanceOf(StringException.class)
+            .hasMessage(COMMON_BLANK_STRING.getMessage());
+  }
+
+  @DisplayName("축사 이름이 10글자를 넘어가면 예외를 발생시킨다.")
+  @Test
+  void createBarnWithOver10Size() {
+    //given // when //then
+    assertThatThrownBy(() -> barnService.createBarn(STRING_OVER_10)).isInstanceOf(StringException.class)
+            .hasMessage(COMMON_STRING_OVER_10.getMessage());
   }
 
 }
