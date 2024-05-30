192f0988bebdb7b5b2b6e5b6c5be519620928e22
choc0villain <sunww2017@naver.com>
2024-05-30 12:17:17 +0900

    test : 노트 등록 api 테스트

---
+++ src/test/java/myproject/mockjang/api/controller/note_parser/NoteParserControllerTest.java
@@ -0,0 +1,64 @@
+package myproject.mockjang.api.controller.note_parser;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+import java.util.Optional;
+import myproject.mockjang.api.controller.note_parser.request.NoteParserCreateRequest;
+import myproject.mockjang.api.service.note_parser.NoteParserService;
+import myproject.mockjang.api.service.note_parser.request.NoteParserCreateServiceRequest;
+import myproject.mockjang.api.service.note_parser.response.NoteParserResponse;
+import myproject.mockjang.domain.mockjang.barn.Barn;
+import myproject.mockjang.domain.mockjang.barn.BarnRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = NoteParserController.class)
+class NoteParserControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @MockBean
+  private NoteParserService noteParserService;
+  protected static final String PARSER_BARN_CODE_ID_1 = "1번축사";
+  protected static final String PARSER_BARN_NOTE_1 = "1번축사 입력 테스트";
+
+  @DisplayName("신규 일지를 저장한다.")
+  @Test
+  void createNote() throws Exception {
+    // given
+    HashMap<String, Integer> names = new HashMap<>();
+    names.put(PARSER_BARN_CODE_ID_1, 1);
+    NoteParserResponse response = NoteParserResponse.builder().names(names).build();
+
+
+    String context = "[["+PARSER_BARN_CODE_ID_1+"]] "+PARSER_BARN_NOTE_1;
+    NoteParserCreateRequest request = NoteParserCreateRequest.builder()
+        .context(context)
+        .build();
+
+    // when // then
+    when(noteParserService.parseNoteAndSaveRecord(any())).thenReturn(response);
+    mockMvc.perform(
+            post("/api/v0/notes/daily/new")
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isOk());
+  }
+}
