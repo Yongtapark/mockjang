6e39a7f4ba1a9c5432f71306cd5996764be271a9
choc0villain <sunww2017@naver.com>
2024-05-30 16:58:18 +0900

    add : 컨트롤러

---
+++ src/main/java/myproject/mockjang/api/controller/mockjang/pen/PenController.java
@@ -0,0 +1,39 @@
+package myproject.mockjang.api.controller.mockjang.pen;
+
+import jakarta.validation.Valid;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import myproject.mockjang.api.ApiResponse;
+import myproject.mockjang.api.controller.mockjang.barn.request.BarnCreateRequest;
+import myproject.mockjang.api.controller.mockjang.pen.request.PenCreateRequest;
+import myproject.mockjang.api.service.mockjang.barn.response.BarnResponse;
+import myproject.mockjang.api.service.mockjang.pen.PenService;
+import myproject.mockjang.api.service.mockjang.pen.response.PenResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PenController {
+
+  private final PenService penService;
+
+  @PostMapping("/api/v0/pens/new")
+  public ApiResponse<PenResponse> createPen(@Valid @RequestBody PenCreateRequest request){
+    return ApiResponse.ok(penService.createPen(request.toServiceRequest()));
+  }
+
+  @GetMapping("/api/v0/pens")
+  public ApiResponse<List<PenResponse>> createBarn(){
+    return ApiResponse.ok(penService.findAll());
+  }
+
+  @GetMapping("/api/v0/pens/{codeId}")
+  public ApiResponse<PenResponse> findByCodeId(@PathVariable String codeId){
+    return ApiResponse.ok(penService.findByCodeId(codeId));
+  }
+
+}
