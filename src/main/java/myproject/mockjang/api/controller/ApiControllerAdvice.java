c268dfcb88f7eb38a9e6130d00a349ead8be0dcf
choc0villain <sunww2017@naver.com>
2024-06-02 03:16:48 +0900

    refactor

--- src/main/java/myproject/mockjang/api/controller/ApiControllerAdvice.java
+++ src/main/java/myproject/mockjang/api/controller/ApiControllerAdvice.java
@@ -1,6 +1,7 @@
 package myproject.mockjang.api.controller;
 
 import myproject.mockjang.api.ApiResponse;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,11 +14,6 @@ public class ApiControllerAdvice {
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   @ExceptionHandler(BindException.class)
   public ApiResponse<Object> bindException(BindException e) {
-    return ApiResponse.of(
-        HttpStatus.BAD_REQUEST,
-        e.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
-        null
-    );
+    return ApiResponse.of(HttpStatus.BAD_REQUEST, e.getBindingResult().getAllErrors().get(0).getDefaultMessage(), null);
   }
-
 }
