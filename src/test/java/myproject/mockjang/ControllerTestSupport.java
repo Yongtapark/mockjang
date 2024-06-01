e13967ca6b4a57825364b447d2eb20d981a24939
choc0villain <sunww2017@naver.com>
2024-06-02 03:16:57 +0900

    refactor

--- src/test/java/myproject/mockjang/ControllerTestSupport.java
+++ src/test/java/myproject/mockjang/ControllerTestSupport.java
@@ -6,11 +6,13 @@ import myproject.mockjang.api.controller.mockjang.cow.CowController;
 import myproject.mockjang.api.controller.mockjang.pen.PenController;
 import myproject.mockjang.api.controller.note_parser.NoteParserController;
 import myproject.mockjang.api.controller.records.mockjang.cow.CowRecordController;
+import myproject.mockjang.api.controller.records.simple_record.SimpleRecordController;
 import myproject.mockjang.api.service.mockjang.barn.BarnService;
 import myproject.mockjang.api.service.mockjang.cow.CowService;
 import myproject.mockjang.api.service.mockjang.pen.PenService;
 import myproject.mockjang.api.service.note_parser.NoteParserService;
 import myproject.mockjang.api.service.records.mockjang.cow.CowRecordService;
+import myproject.mockjang.api.service.records.simple_record.SimpleRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -22,7 +24,8 @@ import org.springframework.test.web.servlet.MockMvc;
     BarnController.class,
     PenController.class,
     CowController.class,
-    CowRecordController.class
+    CowRecordController.class,
+    SimpleRecordController.class
 })
 public abstract class ControllerTestSupport extends TestConstants {
 
@@ -46,4 +49,7 @@ public abstract class ControllerTestSupport extends TestConstants {
 
   @MockBean
   protected CowRecordService cowRecordService;
+
+  @MockBean
+  protected SimpleRecordService simpleRecordService;
 }
