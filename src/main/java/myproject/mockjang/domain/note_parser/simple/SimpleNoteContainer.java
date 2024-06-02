708e95d676748c6e256c30ba6af7829d0263220d
choc0villain <sunww2017@naver.com>
2024-06-02 16:58:58 +0900

    add : 불변 리스트 반환

---
+++ src/main/java/myproject/mockjang/domain/note_parser/simple/SimpleNoteContainer.java
@@ -0,0 +1,18 @@
+package myproject.mockjang.domain.note_parser.simple;
+
+import java.util.List;
+import lombok.Getter;
+import myproject.mockjang.domain.note_parser.NoteContainer;
+import myproject.mockjang.domain.note_parser.mockjang.NoteAndCodeId;
+
+public class SimpleNoteContainer implements NoteContainer {
+  private List<NoteAndCodeId> notes;
+
+  public void registerNoteAndIds(List<NoteAndCodeId> noteAndCodeIds){
+    this.notes = noteAndCodeIds;
+  }
+
+  public List<NoteAndCodeId> getImmutable() {
+    return List.copyOf(notes);
+  }
+}
