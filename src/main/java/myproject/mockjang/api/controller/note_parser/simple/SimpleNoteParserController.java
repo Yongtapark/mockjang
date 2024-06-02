8691a1503d62a80da2593b193af08cf2635b0870
choc0villain <sunww2017@naver.com>
2024-06-02 17:14:18 +0900

    add : 기록 저잦ㅇ

---
+++ src/main/java/myproject/mockjang/api/controller/note_parser/simple/SimpleNoteParserController.java
@@ -0,0 +1,27 @@
+package myproject.mockjang.api.controller.note_parser.simple;
+
+import jakarta.validation.Valid;
+import java.util.HashMap;
+import lombok.RequiredArgsConstructor;
+import myproject.mockjang.api.ApiResponse;
+import myproject.mockjang.api.controller.note_parser.simple.request.SimpleNoteParserCreateRequest;
+import myproject.mockjang.api.service.note_parser.mockjang.response.NoteParserResponse;
+import myproject.mockjang.api.service.note_parser.simple.SimpleNoteParserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class SimpleNoteParserController {
+
+  private final SimpleNoteParserService noteParserService;
+
+  @PostMapping("/api/v0/simple_records/new")
+  public ApiResponse<NoteParserResponse> createRecords(
+      @Valid @RequestBody SimpleNoteParserCreateRequest request) {
+    return ApiResponse.ok(
+        noteParserService.parseNoteAndSaveRecord(request.toServiceRequest(new HashMap<>())));
+  }
+
+}
