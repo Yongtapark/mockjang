aada15f23bf2ede8dd373cd57113228e783b51f1
choc0villain <sunww2017@naver.com>
2024-06-08 23:00:49 +0900

    refactor

--- src/main/java/myproject/mockjang/config/WebConfig.java
+++ src/main/java/myproject/mockjang/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:52618")
+            .allowedOrigins("http://localhost:8082")
             .allowedMethods("POST", "GET", "PUT");
       }
     };
