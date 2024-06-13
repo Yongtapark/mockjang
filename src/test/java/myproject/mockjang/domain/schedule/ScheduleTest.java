fe377064b0093ae29afd869862ea23f2621b8188
Yong Ta Park <qkrdydxk80@nate.com>
2024-06-13 15:19:58 +0900

    test : 스케쥴 범위에 대한 케이스, 예외 케이스

--- src/test/java/myproject/mockjang/domain/schedule/ScheduleTest.java
+++ src/test/java/myproject/mockjang/domain/schedule/ScheduleTest.java
@@ -1,6 +1,7 @@
 package myproject.mockjang.domain.schedule;
 
 import myproject.mockjang.IntegrationTestSupport;
+import myproject.mockjang.exception.schdules.ScheduleFormException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -62,6 +63,46 @@ class ScheduleTest extends IntegrationTestSupport {
 
     //then
     Assertions.assertThat(schedule.getScheduleStatus()).isEqualTo(ScheduleStatus.IN_PROGRESS);
+  }
+
+  @DisplayName("읽은 날짜 기준 시작 날짜가 지나지 않았꼬, 목표 날짜를 지나지 않았다면 스케줄 타입이 '예정' 이다.")
+  @Test
+  void whenStartDateBeforeTargetDateBefore() {
+    //given
+    Schedule schedule = Schedule.builder().targetDate(READ_DATE_AFTER)
+            .startDate(READ_DATE_AFTER).build();
+
+    //when
+    schedule.calculateScheduleType(READ_DATE);
+
+    //then
+    Assertions.assertThat(schedule.getScheduleStatus()).isEqualTo(ScheduleStatus.UPCOMING);
+  }
+
+  @DisplayName("읽은 날짜 기준 시작 날짜가 지났고, 목표 날짜를 지났다면 스케줄 타입이 '만료' 이다.")
+  @Test
+  void whenStartDateAfterTargetDateAfter() {
+    //given
+    Schedule schedule = Schedule.builder().targetDate(TARGET_DATE_AFTER_READ)
+            .startDate(START_DATE_BEFORE_READ).build();
+
+    //when
+    schedule.calculateScheduleType(READ_DATE);
+
+    //then
+    Assertions.assertThat(schedule.getScheduleStatus()).isEqualTo(ScheduleStatus.EXPIRED);
+  }
+
+  @DisplayName("목표 날짜보다 시작 날짜가 늦으면 예외를 발생시킨다.")
+  @Test
+  void whenStartDateLateThenTargetDate() {
+    //given
+    Schedule schedule = Schedule.builder().targetDate(READ_DATE)
+            .startDate(READ_DATE_AFTER).build();
+
+    //when //then
+    Assertions.assertThatThrownBy(()->schedule.calculateScheduleType(READ_DATE)).isInstanceOf(ScheduleFormException.class)
+            .hasMessage("시작 날짜보다 목표 날짜가 시간상 뒤에 위치할 수 없습니다.");
   }
 
 }
