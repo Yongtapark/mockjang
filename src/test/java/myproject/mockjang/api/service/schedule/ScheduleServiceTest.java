09588a2b8f637b4966f93d3a9d1aba8b77305246
choc0villain <sunww2017@naver.com>
2024-06-14 01:40:47 +0900

    add : 생성, 수정, 제거, 금주일정 조회, 검색 기능 폼

---
+++ src/test/java/myproject/mockjang/api/service/schedule/ScheduleServiceTest.java
@@ -0,0 +1,61 @@
+package myproject.mockjang.api.service.schedule;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import myproject.mockjang.IntegrationTestSupport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ScheduleServiceTest extends IntegrationTestSupport {
+
+  @DisplayName("일정을 생성한다.")
+  @Test
+  void create() {
+      //given
+
+      //when
+
+      //then
+  }
+
+  @DisplayName("일정을 수정한다.")
+  @Test
+  void update() {
+      //given
+
+      //when
+
+      //then
+  }
+
+  @DisplayName("일정을 제거한다.")
+  @Test
+  void delete() {
+      //given
+
+      //when
+
+      //then
+  }
+
+  @DisplayName("조회일 부터 조회일 주말까지의 일정을 조회한다.")
+  @Test
+  void test() {
+      //given
+
+      //when
+
+      //then
+  }
+
+  @DisplayName("일정을 검색한다.")
+  @Test
+  void search() {
+      //given
+
+      //when
+
+      //then
+  }
+
+}
