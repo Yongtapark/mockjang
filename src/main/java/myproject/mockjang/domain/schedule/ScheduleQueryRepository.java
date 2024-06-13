e83eea9de3fdd82489901b90b4cc6bb4b81a23f1
choc0villain <sunww2017@naver.com>
2024-06-14 01:38:26 +0900

    refactor

--- src/main/java/myproject/mockjang/domain/schedule/ScheduleQueryRepository.java
+++ src/main/java/myproject/mockjang/domain/schedule/ScheduleQueryRepository.java
@@ -1,12 +1,15 @@
 package myproject.mockjang.domain.schedule;
 
 import static java.time.DayOfWeek.SUNDAY;
+import static myproject.mockjang.domain.records.simple.QSimpleRecord.simpleRecord;
 import static myproject.mockjang.domain.schedule.QSchedule.schedule;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import myproject.mockjang.domain.records.RecordType;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -21,5 +24,29 @@ public class ScheduleQueryRepository {
                 .where(schedule.startDate.loe(endOfWeek).and(schedule.targetDate.goe(date)))
                 .orderBy(schedule.targetDate.asc(),schedule.startDate.asc())
                 .fetch();
+    }
+
+    public List<Schedule> search(String context, ScheduleStatus scheduleStatus,
+        LocalDateTime targetDate,LocalDateTime startDate){
+        BooleanBuilder predict = searching(context, scheduleStatus, targetDate, startDate);
+        return queryFactory.selectFrom(schedule).where(predict).fetch();
+    }
+
+    private BooleanBuilder searching(String context, ScheduleStatus scheduleStatus,
+        LocalDateTime targetDate,LocalDateTime startDate) {
+        BooleanBuilder predicate = new BooleanBuilder();
+        if (context != null && !context.isEmpty()) {
+            predicate.and(schedule.context.like("%" + context + "%"));
+        }
+        if (scheduleStatus != null) {
+            predicate.and(schedule.scheduleStatus.eq(scheduleStatus));
+        }
+        if (startDate != null) {
+            predicate.and(schedule.targetDate.eq(targetDate));
+        }
+        if (startDate != null) {
+            predicate.and(schedule.startDate.eq(startDate));
+        }
+        return predicate;
     }
 }
