929850666265e4a9130ea83eb291d717303d3a9c
choc0villain <sunww2017@naver.com>
2024-06-14 15:07:34 +0900

    add : 만료 제외 예정 조회

--- src/main/java/myproject/mockjang/domain/schedule/ScheduleQueryRepository.java
+++ src/main/java/myproject/mockjang/domain/schedule/ScheduleQueryRepository.java
@@ -3,6 +3,7 @@ package myproject.mockjang.domain.schedule;
 import static java.time.DayOfWeek.SUNDAY;
 import static myproject.mockjang.domain.records.simple.QSimpleRecord.simpleRecord;
 import static myproject.mockjang.domain.schedule.QSchedule.schedule;
+import static myproject.mockjang.domain.schedule.ScheduleStatus.*;
 
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -15,38 +16,45 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RequiredArgsConstructor
 public class ScheduleQueryRepository {
-    private final JPAQueryFactory queryFactory;
 
-    public List<Schedule> findAllScheduleThisDayToEndOfWeek(LocalDateTime date){
-        LocalDateTime endOfWeek = date.with(SUNDAY);
-        return queryFactory
-                .selectFrom(schedule)
-                .where(schedule.startDate.loe(endOfWeek).and(schedule.targetDate.goe(date)))
-                .orderBy(schedule.targetDate.asc(),schedule.startDate.asc())
-                .fetch();
-    }
+  private final JPAQueryFactory queryFactory;
 
-    public List<Schedule> search(String context, ScheduleStatus scheduleStatus,
-        LocalDateTime targetDate,LocalDateTime startDate){
-        BooleanBuilder predict = searching(context, scheduleStatus, targetDate, startDate);
-        return queryFactory.selectFrom(schedule).where(predict).fetch();
-    }
+  public List<Schedule> findAllScheduleExceptExpired() {
+    return queryFactory.selectFrom(schedule)
+        .where(schedule.scheduleStatus.ne(EXPIRED))
+        .fetch();
+  }
 
-    private BooleanBuilder searching(String context, ScheduleStatus scheduleStatus,
-        LocalDateTime targetDate,LocalDateTime startDate) {
-        BooleanBuilder predicate = new BooleanBuilder();
-        if (context != null && !context.isEmpty()) {
-            predicate.and(schedule.context.like("%" + context + "%"));
-        }
-        if (scheduleStatus != null) {
-            predicate.and(schedule.scheduleStatus.eq(scheduleStatus));
-        }
-        if (targetDate != null) {
-            predicate.and(schedule.targetDate.eq(targetDate));
-        }
-        if (startDate != null) {
-            predicate.and(schedule.startDate.eq(startDate));
-        }
-        return predicate;
+  public List<Schedule> findAllScheduleThisDayToEndOfWeek(LocalDateTime date) {
+    LocalDateTime endOfWeek = date.with(SUNDAY);
+    return queryFactory
+        .selectFrom(schedule)
+        .where(schedule.startDate.loe(endOfWeek).and(schedule.targetDate.goe(date)))
+        .orderBy(schedule.targetDate.asc(), schedule.startDate.asc())
+        .fetch();
+  }
+
+  public List<Schedule> search(LocalDateTime startDate, LocalDateTime targetDate,
+      String context, ScheduleStatus scheduleStatus) {
+    BooleanBuilder predict = searching(context, scheduleStatus, targetDate, startDate);
+    return queryFactory.selectFrom(schedule).where(predict).fetch();
+  }
+
+  private BooleanBuilder searching(String context, ScheduleStatus scheduleStatus,
+      LocalDateTime targetDate, LocalDateTime startDate) {
+    BooleanBuilder predicate = new BooleanBuilder();
+    if (context != null && !context.isEmpty()) {
+      predicate.and(schedule.context.like("%" + context + "%"));
     }
+    if (scheduleStatus != null) {
+      predicate.and(schedule.scheduleStatus.eq(scheduleStatus));
+    }
+    if (targetDate != null) {
+      predicate.and(schedule.targetDate.eq(targetDate));
+    }
+    if (startDate != null) {
+      predicate.and(schedule.startDate.eq(startDate));
+    }
+    return predicate;
+  }
 }
