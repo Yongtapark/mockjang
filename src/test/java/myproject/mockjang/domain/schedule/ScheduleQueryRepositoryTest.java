2258aeb408c3d71728f7fca46b0a1a31fcfbbec8
choc0villain <sunww2017@naver.com>
2024-06-14 15:07:23 +0900

    test : 만료 제외 예정 조회, 검색

--- src/test/java/myproject/mockjang/domain/schedule/ScheduleQueryRepositoryTest.java
+++ src/test/java/myproject/mockjang/domain/schedule/ScheduleQueryRepositoryTest.java
@@ -1,5 +1,6 @@
 package myproject.mockjang.domain.schedule;
 
+import static myproject.mockjang.domain.schedule.ScheduleStatus.EXPIRED;
 import static org.assertj.core.api.Assertions.*;
 
 import java.time.DayOfWeek;
@@ -11,31 +12,180 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 class ScheduleQueryRepositoryTest extends IntegrationTestSupport {
-    @Autowired
-    private ScheduleQueryRepository scheduleQueryRepository;
-    @Autowired
-    private ScheduleRepository scheduleRepository;
 
-    @DisplayName("조회 날짜 부터, 해당 주 일요일 까지의 일정을 조회한다.")
-    @Test
-    void findAllScheduleThisDayToEndOfWeek() {
-        //given
-        LocalDateTime startDayOfWeek = READ_DATE.with(DayOfWeek.MONDAY);
-        LocalDateTime endDayOfWeek = READ_DATE.with(DayOfWeek.SUNDAY);
+  @Autowired
+  private ScheduleQueryRepository scheduleQueryRepository;
+  @Autowired
+  private ScheduleRepository scheduleRepository;
 
-        Schedule schedule1 = Schedule.builder().startDate(READ_DATE.minusDays(7)).targetDate(READ_DATE).build();
-        Schedule schedule2 = Schedule.builder().startDate(READ_DATE).targetDate(READ_DATE.plusDays(7)).build();
-        Schedule schedule3 = Schedule.builder().startDate(READ_DATE.minusDays(7)).targetDate(READ_DATE.plusDays(7)).build();
-        Schedule schedule4 = Schedule.builder().startDate(READ_DATE.minusDays(7)).targetDate(READ_DATE.minusDays(7)).build();
-        Schedule schedule5 = Schedule.builder().startDate(READ_DATE.plusDays(7)).targetDate(READ_DATE.plusDays(7)).build();
-        scheduleRepository.saveAll(List.of(schedule1, schedule2, schedule3, schedule4,schedule5));
+  @DisplayName("조회 날짜 부터, 해당 주 일요일 까지의 일정을 조회한다.")
+  @Test
+  void findAllScheduleThisDayToEndOfWeek() {
+    //given
+    Schedule schedule1 = Schedule.builder().startDate(READ_DATE.minusDays(7)).targetDate(READ_DATE)
+        .build();
+    Schedule schedule2 = Schedule.builder().startDate(READ_DATE).targetDate(READ_DATE.plusDays(7))
+        .build();
+    Schedule schedule3 = Schedule.builder().startDate(READ_DATE.minusDays(7))
+        .targetDate(READ_DATE.plusDays(7)).build();
+    Schedule schedule4 = Schedule.builder().startDate(READ_DATE.minusDays(7))
+        .targetDate(READ_DATE.minusDays(7)).build();
+    Schedule schedule5 = Schedule.builder().startDate(READ_DATE.plusDays(7))
+        .targetDate(READ_DATE.plusDays(7)).build();
+    scheduleRepository.saveAll(List.of(schedule1, schedule2, schedule3, schedule4, schedule5));
+
+    //when
+    List<Schedule> allScheduleThisWeek = scheduleQueryRepository.findAllScheduleThisDayToEndOfWeek(
+        READ_DATE);
+
+    //then
+    assertThat(allScheduleThisWeek).containsExactly(schedule1, schedule3, schedule2);
+    assertThat(allScheduleThisWeek).doesNotContain(schedule4, schedule5);
+  }
+
+  @DisplayName("시작일을 기준으로 일정을 검색한다.")
+  @Test
+  void searchWithStartDate() {
+    //given
+    Schedule schedule1 = Schedule.builder().startDate(READ_DATE.minusDays(7)).targetDate(READ_DATE)
+        .build();
+    Schedule schedule2 = Schedule.builder().startDate(READ_DATE).targetDate(READ_DATE.plusDays(7))
+        .build();
+    Schedule schedule3 = Schedule.builder().startDate(READ_DATE.minusDays(7))
+        .targetDate(READ_DATE.plusDays(7)).build();
+    Schedule schedule4 = Schedule.builder().startDate(READ_DATE.minusDays(7))
+        .targetDate(READ_DATE.minusDays(7)).build();
+    Schedule schedule5 = Schedule.builder().startDate(READ_DATE.plusDays(7))
+        .targetDate(READ_DATE.plusDays(7)).build();
+    scheduleRepository.saveAll(List.of(schedule1, schedule2, schedule3, schedule4, schedule5));
+
+    //when
+    List<Schedule> searches = scheduleQueryRepository.search(READ_DATE, null, null, null);
+
+    //then
+    assertThat(searches).containsOnly(schedule2);
+  }
+
+  @DisplayName("목표일을 기준으로 일정을 검색한다.")
+  @Test
+  void searchWithTargetDate() {
+    //given
+    Schedule schedule1 = Schedule.builder().startDate(READ_DATE.minusDays(7)).targetDate(READ_DATE)
+        .build();
+    Schedule schedule2 = Schedule.builder().startDate(READ_DATE).targetDate(READ_DATE.plusDays(7))
+        .build();
+    Schedule schedule3 = Schedule.builder().startDate(READ_DATE.minusDays(7))
+        .targetDate(READ_DATE.plusDays(7)).build();
+    Schedule schedule4 = Schedule.builder().startDate(READ_DATE.minusDays(7))
+        .targetDate(READ_DATE.minusDays(7)).build();
+    Schedule schedule5 = Schedule.builder().startDate(READ_DATE.plusDays(7))
+        .targetDate(READ_DATE.plusDays(7)).build();
+    scheduleRepository.saveAll(List.of(schedule1, schedule2, schedule3, schedule4, schedule5));
+
+    //when
+    List<Schedule> searches = scheduleQueryRepository.search(null, READ_DATE, null, null);
+
+    //then
+    assertThat(searches).containsOnly(schedule1);
+  }
+
+  @DisplayName("내용을 기준으로 일정을 검색한다.")
+  @Test
+  void searchWithContext() {
+    //given
+    Schedule schedule1 = Schedule.builder().startDate(READ_DATE.minusDays(7)).targetDate(READ_DATE)
+        .context(SCHEDULE_CONTEXT_1)
+        .build();
+    Schedule schedule2 = Schedule.builder().startDate(READ_DATE).targetDate(READ_DATE.plusDays(7))
+        .context(SCHEDULE_CONTEXT_2)
+        .build();
+    Schedule schedule3 = Schedule.builder().startDate(READ_DATE.minusDays(7))
+        .context(SCHEDULE_CONTEXT_2)
+        .targetDate(READ_DATE.plusDays(7)).build();
+    Schedule schedule4 = Schedule.builder().startDate(READ_DATE.minusDays(7))
+        .context(SCHEDULE_CONTEXT_2)
+        .targetDate(READ_DATE.minusDays(7)).build();
+    Schedule schedule5 = Schedule.builder().startDate(READ_DATE.plusDays(7))
+        .context(SCHEDULE_CONTEXT_2)
+        .targetDate(READ_DATE.plusDays(7)).build();
+    scheduleRepository.saveAll(List.of(schedule1, schedule2, schedule3, schedule4, schedule5));
+
+    //when
+    List<Schedule> searches = scheduleQueryRepository.search(null, null, SCHEDULE_CONTEXT_1, null);
+
+    //then
+    assertThat(searches).containsOnly(schedule1);
+  }
+
+  @DisplayName("일정 상태를 기준으로 일정을 검색한다.")
+  @Test
+  void searchWithScheduleStatus() {
+    //given
+    Schedule schedule1 = Schedule.builder().startDate(READ_DATE.minusDays(7)).targetDate(READ_DATE)
+        .context(SCHEDULE_CONTEXT_1)
+        .build();
+    Schedule schedule2 = Schedule.builder().startDate(READ_DATE).targetDate(READ_DATE.plusDays(7))
+        .context(SCHEDULE_CONTEXT_2)
+        .build();
+    Schedule schedule3 = Schedule.builder().startDate(READ_DATE.minusDays(7))
+        .context(SCHEDULE_CONTEXT_2)
+        .targetDate(READ_DATE.plusDays(7)).build();
+    Schedule schedule4 = Schedule.builder().startDate(READ_DATE.minusDays(7))
+        .context(SCHEDULE_CONTEXT_2)
+        .targetDate(READ_DATE.minusDays(7)).build();
+    Schedule schedule5 = Schedule.builder().startDate(READ_DATE.plusDays(7))
+        .context(SCHEDULE_CONTEXT_2)
+        .targetDate(READ_DATE.plusDays(7)).build();
+
+    schedule1.calculateScheduleType(READ_DATE);
+    schedule2.calculateScheduleType(READ_DATE);
+    schedule3.calculateScheduleType(READ_DATE);
+    schedule4.calculateScheduleType(READ_DATE);
+    schedule5.calculateScheduleType(READ_DATE);
+
+    scheduleRepository.saveAll(List.of(schedule1, schedule2, schedule3, schedule4, schedule5));
+
+    //when
+    List<Schedule> searches = scheduleQueryRepository.search(null, null, null, EXPIRED);
+
+    //then
+    assertThat(searches).containsOnly(schedule4);
+  }
 
 
-        //when
-        List<Schedule> allScheduleThisWeek = scheduleQueryRepository.findAllScheduleThisDayToEndOfWeek(READ_DATE);
+  @DisplayName("만료된 일정을 제외한 일정들을 조회한다.")
+  @Test
+  void findAllScheduleExceptExpired() {
+    //given
+    Schedule schedule1 = Schedule.builder().startDate(READ_DATE.minusDays(7)).targetDate(READ_DATE)
+        .context(SCHEDULE_CONTEXT_1)
+        .build();
+    Schedule schedule2 = Schedule.builder().startDate(READ_DATE).targetDate(READ_DATE.plusDays(7))
+        .context(SCHEDULE_CONTEXT_2)
+        .build();
+    Schedule schedule3 = Schedule.builder().startDate(READ_DATE.minusDays(7))
+        .context(SCHEDULE_CONTEXT_2)
+        .targetDate(READ_DATE.plusDays(7)).build();
+    Schedule schedule4 = Schedule.builder().startDate(READ_DATE.minusDays(7))
+        .context(SCHEDULE_CONTEXT_2)
+        .targetDate(READ_DATE.minusDays(7)).build();
+    Schedule schedule5 = Schedule.builder().startDate(READ_DATE.plusDays(7))
+        .context(SCHEDULE_CONTEXT_2)
+        .targetDate(READ_DATE.plusDays(7)).build();
 
-        //then
-        assertThat(allScheduleThisWeek).containsExactly(schedule1,schedule3,schedule2);
-        assertThat(allScheduleThisWeek).doesNotContain(schedule4,schedule5);
-    }
+    schedule1.calculateScheduleType(READ_DATE);
+    schedule2.calculateScheduleType(READ_DATE);
+    schedule3.calculateScheduleType(READ_DATE);
+    schedule4.calculateScheduleType(READ_DATE);
+    schedule5.calculateScheduleType(READ_DATE);
+
+    scheduleRepository.saveAll(List.of(schedule1, schedule2, schedule3, schedule4, schedule5));
+
+    //when
+    List<Schedule> schedules = scheduleQueryRepository.findAllScheduleExceptExpired();
+
+    //then
+    assertThat(schedules).containsOnly(schedule1, schedule2, schedule3, schedule5);
+    assertThat(schedules).doesNotContain(schedule4);
+  }
 }
