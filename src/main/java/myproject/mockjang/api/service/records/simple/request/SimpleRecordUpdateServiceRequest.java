9fd4c89c3eac9486f7a713bf211cbcb7f8473319
choc0villain <sunww2017@naver.com>
2024-06-14 12:04:45 +0900

    add : dto

---
+++ src/main/java/myproject/mockjang/api/service/records/simple/request/SimpleRecordUpdateServiceRequest.java
@@ -0,0 +1,27 @@
+package myproject.mockjang.api.service.records.simple.request;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import myproject.mockjang.domain.records.RecordType;
+
+@Getter
+@NoArgsConstructor
+public class SimpleRecordUpdateServiceRequest {
+  private Long id;
+  private String codeId;
+  private RecordType recordType;
+  private LocalDateTime date;
+  private String record;
+
+  @Builder
+  private SimpleRecordUpdateServiceRequest(Long id, String codeId, RecordType recordType,
+      LocalDateTime date, String record) {
+    this.id = id;
+    this.codeId = codeId;
+    this.recordType = recordType;
+    this.date = date;
+    this.record = record;
+  }
+}
