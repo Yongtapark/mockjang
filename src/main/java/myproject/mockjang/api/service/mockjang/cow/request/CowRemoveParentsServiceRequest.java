ad1bb1adbd86c73ecaa46e323c3188c8dc4cba18
Yong Ta Park <qkrdydxk80@nate.com>
2024-06-25 18:55:45 +0900

    refactor : 필드타입 변경

--- src/main/java/myproject/mockjang/api/service/mockjang/cow/request/CowRemoveParentsServiceRequest.java
+++ src/main/java/myproject/mockjang/api/service/mockjang/cow/request/CowRemoveParentsServiceRequest.java
@@ -4,17 +4,16 @@ import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import myproject.mockjang.domain.mockjang.cow.Cow;
 
 @Getter
 @NoArgsConstructor
 public class CowRemoveParentsServiceRequest {
-    private Cow cow;
-    private List<Cow> parents;
+    private Long cowId;
+    private List<Long> parentsIds;
 
     @Builder
-    private CowRemoveParentsServiceRequest(Cow cow, List<Cow> parents) {
-        this.cow = cow;
-        this.parents = parents;
+    private CowRemoveParentsServiceRequest(Long cowId, List<Long> parentsIds) {
+        this.cowId = cowId;
+        this.parentsIds = parentsIds;
     }
 }
