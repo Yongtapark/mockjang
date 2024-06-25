4c80c7419febc5ab64c9e3d42ce4dca07f85e364
Yong Ta Park <qkrdydxk80@nate.com>
2024-06-25 15:05:31 +0900

    add : 부모 자식 관계 제거

--- src/main/java/myproject/mockjang/domain/mockjang/cow/Cow.java
+++ src/main/java/myproject/mockjang/domain/mockjang/cow/Cow.java
@@ -1,6 +1,7 @@
 package myproject.mockjang.domain.mockjang.cow;
 
 import static myproject.mockjang.exception.Exceptions.COMMON_ALREADY_EXIST;
+import static myproject.mockjang.exception.Exceptions.COMMON_NOT_EXIST;
 import static myproject.mockjang.exception.Exceptions.COMMON_NO_UNDER_GROUP;
 import static myproject.mockjang.exception.Exceptions.COMMON_NO_UPPER_GROUP;
 import static myproject.mockjang.exception.Exceptions.DOMAIN_ONLY_SLAUGHTERED_ERROR;
@@ -28,6 +29,7 @@ import myproject.mockjang.domain.mockjang.Mockjang;
 import myproject.mockjang.domain.mockjang.barn.Barn;
 import myproject.mockjang.domain.mockjang.pen.Pen;
 import myproject.mockjang.domain.records.mockjang.cow.CowRecord;
+import myproject.mockjang.exception.common.NotExistException;
 import myproject.mockjang.exception.common.ThereIsNoGroupException;
 import myproject.mockjang.exception.common.UpperGroupAlreadyExistException;
 import myproject.mockjang.exception.cow.CowStatusException;
@@ -43,154 +45,169 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 public class Cow implements Mockjang {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  private String codeId;
+    private String codeId;
 
-  private LocalDateTime birthDate;
+    private LocalDateTime birthDate;
 
-  @Enumerated(EnumType.STRING)
-  private Gender gender;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
 
-  @Enumerated(EnumType.STRING)
-  private CowStatus cowStatus;
+    @Enumerated(EnumType.STRING)
+    private CowStatus cowStatus;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  private Barn barn;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Barn barn;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  private Pen pen;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Pen pen;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "mom_id")
-  private Cow mom;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mom_id")
+    private Cow mom;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "dad_id")
-  private Cow dad;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dad_id")
+    private Cow dad;
 
-  @OneToMany(mappedBy = "mom")
-  private final List<Cow> children = new ArrayList<>();
+    @OneToMany(mappedBy = "mom")
+    private final List<Cow> children = new ArrayList<>();
 
-  @OneToMany(mappedBy = "cow")
-  private List<FeedConsumption> feedConsumptions = new ArrayList<>();
+    @OneToMany(mappedBy = "cow")
+    private List<FeedConsumption> feedConsumptions = new ArrayList<>();
 
-  @OneToMany(mappedBy = "cow")
-  private List<CowRecord> records = new ArrayList<>();
+    @OneToMany(mappedBy = "cow")
+    private List<CowRecord> records = new ArrayList<>();
 
-  private Integer unitPrice;
+    private Integer unitPrice;
 
-  private boolean deleted = false;
+    private boolean deleted = false;
 
-  @Builder
-  private Cow(String codeId, LocalDateTime birthDate, Gender gender, Barn barn, Pen pen,
-      CowStatus cowStatus,
-      List<FeedConsumption> feedConsumptions, List<CowRecord> records, Integer unitPrice) {
+    @Builder
+    private Cow(String codeId, LocalDateTime birthDate, Gender gender, Barn barn, Pen pen,
+                CowStatus cowStatus,
+                List<FeedConsumption> feedConsumptions, List<CowRecord> records, Integer unitPrice) {
 
-    this.codeId = codeId;
-    this.birthDate = birthDate;
-    this.gender = gender;
-    this.barn = barn;
-    this.pen = pen;
-    this.cowStatus = cowStatus;
-    this.unitPrice = unitPrice;
-    if (feedConsumptions != null) {
-      this.feedConsumptions = feedConsumptions;
+        this.codeId = codeId;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.barn = barn;
+        this.pen = pen;
+        this.cowStatus = cowStatus;
+        this.unitPrice = unitPrice;
+        if (feedConsumptions != null) {
+            this.feedConsumptions = feedConsumptions;
+        }
+        if (records != null) {
+            this.records = records;
+        }
     }
-    if (records != null) {
-      this.records = records;
+
+    public static Cow createCow(String cowCode, Gender gender, CowStatus cowStatus,
+                                LocalDateTime birthDate) {
+        return Cow.builder()
+                .codeId(cowCode)
+                .birthDate(birthDate)
+                .gender(gender)
+                .cowStatus(cowStatus)
+                .build();
     }
-  }
 
-  public static Cow createCow(String cowCode, Gender gender, CowStatus cowStatus,
-      LocalDateTime birthDate) {
-    return Cow.builder()
-        .codeId(cowCode)
-        .birthDate(birthDate)
-        .gender(gender)
-        .cowStatus(cowStatus)
-        .build();
-  }
-
-  public void registerBarn(Barn barn) {
-    if (this.barn != null) {
-      throw new UpperGroupAlreadyExistException(barn, COMMON_ALREADY_EXIST);
+    public void registerBarn(Barn barn) {
+        if (this.barn != null) {
+            throw new UpperGroupAlreadyExistException(barn, COMMON_ALREADY_EXIST);
+        }
+        this.barn = barn;
     }
-    this.barn = barn;
-  }
 
-  private void changeBarn(Barn barn) {
-    this.barn = barn;
-  }
-
-  @Override
-  public void registerUpperGroup(Mockjang upperGroup) {
-    if (upperGroup instanceof Pen pen) {
-      if (this.pen != null) {
-        throw new UpperGroupAlreadyExistException(pen, COMMON_ALREADY_EXIST);
-      }
-      this.pen = pen;
-      pen.addCow(this);
+    private void changeBarn(Barn barn) {
+        this.barn = barn;
     }
-  }
 
-  @Override
-  public void changeUpperGroup(Mockjang mockjang) {
-    if (mockjang instanceof Pen pen) {
-      this.pen.deleteCow(this);
-      this.pen = null;
-      changeBarn(pen.getBarn());
-      registerUpperGroup(pen);
+    @Override
+    public void registerUpperGroup(Mockjang upperGroup) {
+        if (upperGroup instanceof Pen pen) {
+            if (this.pen != null) {
+                throw new UpperGroupAlreadyExistException(pen, COMMON_ALREADY_EXIST);
+            }
+            this.pen = pen;
+            pen.addCow(this);
+        }
     }
-  }
 
-  public void registerAllChildren(List<Cow> children) {
-    for (Cow child : children) {
-      child.registerParent(this);
+    @Override
+    public void changeUpperGroup(Mockjang mockjang) {
+        if (mockjang instanceof Pen pen) {
+            this.pen.removeOneOfUnderGroups(this);
+            this.pen = null;
+            changeBarn(pen.getBarn());
+            registerUpperGroup(pen);
+        }
     }
-  }
 
-  public void registerParent(Cow parent) {
-    if (parent.gender.equals(Gender.FEMALE)) {
-      this.mom = parent;
-    } else {
-      this.dad = parent;
+    public void registerAllChildren(List<Cow> children) {
+        for (Cow child : children) {
+            child.registerParent(this);
+        }
     }
-    parent.children.add(this);
-  }
 
-  public void registerRecord(CowRecord record) {
-    records.add(record);
-  }
-
-  public void registerFeedConsumptions(FeedConsumption feedConsumption) {
-    feedConsumptions.add(feedConsumption);
-  }
-
-  public void changeCowStatus(CowStatus cowStatus) {
-    this.cowStatus = cowStatus;
-  }
-
-  public void registerUnitPrice(Integer unitPrice) {
-    if (getCowStatus() == null || !getCowStatus().equals(CowStatus.SLAUGHTERED)) {
-      throw new CowStatusException(DOMAIN_ONLY_SLAUGHTERED_ERROR);
+    public void registerParent(Cow parent) {
+        if (parent.gender.equals(Gender.FEMALE)) {
+            this.mom = parent;
+        } else {
+            this.dad = parent;
+        }
+        parent.children.add(this);
     }
-    this.unitPrice = unitPrice;
-  }
 
-  @Override
-  public Mockjang getUpperGroup() {
-    if (pen == null) {
-      throw new ThereIsNoGroupException(COMMON_NO_UPPER_GROUP, this);
+    public void removeParent(Cow parent) {
+        if (parent.gender.equals(Gender.FEMALE)) {
+            this.mom.removeChild(this);
+            this.mom=null;
+            return;
+        }
+        this.dad.removeChild(this);
+        this.dad=null;
     }
-    return pen;
-  }
 
-  @Override
-  public void removeOneOfUnderGroups(Mockjang mockjang) {
-    throw new ThereIsNoGroupException(COMMON_NO_UNDER_GROUP, this);
-  }
+    public void registerRecord(CowRecord record) {
+        records.add(record);
+    }
 
+    public void registerFeedConsumptions(FeedConsumption feedConsumption) {
+        feedConsumptions.add(feedConsumption);
+    }
+
+    public void changeCowStatus(CowStatus cowStatus) {
+        this.cowStatus = cowStatus;
+    }
+
+    public void registerUnitPrice(Integer unitPrice) {
+        if (getCowStatus() == null || !getCowStatus().equals(CowStatus.SLAUGHTERED)) {
+            throw new CowStatusException(DOMAIN_ONLY_SLAUGHTERED_ERROR);
+        }
+        this.unitPrice = unitPrice;
+    }
+
+    @Override
+    public Mockjang getUpperGroup() {
+        if (pen == null) {
+            throw new ThereIsNoGroupException(COMMON_NO_UPPER_GROUP, this);
+        }
+        return pen;
+    }
+
+    @Override
+    public void removeOneOfUnderGroups(Mockjang mockjang) {
+        throw new ThereIsNoGroupException(COMMON_NO_UNDER_GROUP, this);
+    }
+
+    public void removeChild(Cow child) {
+        if(!children.remove(child)){
+            throw new NotExistException(COMMON_NOT_EXIST, child.getCodeId());
+        }
+    }
 }
